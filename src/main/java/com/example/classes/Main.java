e4296f9eda9ccde94d8ea7824146518c5c28b986
Dyestrm <darkyissus29@gmail.com>
2025-11-19 14:14:59 +0800

    fix: Object deserialization issues

--- src/main/java/com/example/classes/Main.java
+++ src/main/java/com/example/classes/Main.java
@@ -2,11 +2,6 @@ package com.example.classes;
 
 public class Main {
     public static void main(String[] args) {        
-        // Customer c = new Customer("a", "b");
-        // c.login();
-
-        //UIManager.displayLoginChoice();
-        //UIManager.displayAdminMenu();
-        Pharmacy admin = AuthService.logInPharmacy("jmpharmacy", "jm123");
+        UIManager.displayLoginChoice();
     }
 }
--- src/main/java/com/example/classes/Pharmacy.java
+++ src/main/java/com/example/classes/Pharmacy.java
@@ -4,16 +4,30 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Pharmacy extends Account {
+
     // Necessary initializations
-    private List<Medicine> medicines = new ArrayList<>();
+    private List<Medicine> medicines;
 
     // TODO: these are just test files, make them more adaptable later
     private Path permanentFile = Path.of(Account.ROOT_DIRECTORY, "pharmacies", "JmPharmacy.json");
     private Path temporaryFile = Path.of(permanentFile.toString() + ".tmp"); // temporary filepath
 
-    // CRUD Methods
+    @JsonCreator
+    Pharmacy(
+        @JsonProperty("name") String name, 
+        @JsonProperty("username") String username, 
+        @JsonProperty("password") String password,
+        @JsonProperty("medicines") List<Medicine> medicines
+    ) {
+        super(name, username, password);
+        this.medicines = medicines;
+    }
 
+    // CRUD Methods
     public void addMedicine() {
         // Prompt line and Input validation
         System.out.println("Enter medicine details.");
