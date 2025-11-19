cce860c700f8e9f68d530fae816aabc2985b64c7
Dyestrm <darkyissus29@gmail.com>
2025-11-19 19:03:29 +0800

    test: Test display medicines

--- src/main/java/com/example/classes/Main.java
+++ src/main/java/com/example/classes/Main.java
@@ -1,7 +1,24 @@
 package com.example.classes;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {        
-        UIManager.displayLoginChoice();
+        // Customer c = new Customer("a", "b");
+        // c.login();
+
+        //UIManager.displayLoginChoice();
+        //UIManager.displayAdminMenu();
+        Pharmacy pharmacy;
+        Path path = Path.of("accounts/pharmacies/HealthPlusPharmacy.json");
+        try {
+            pharmacy = Database.getObjectMapper().readValue(path.toFile(), Pharmacy.class);
+            List<Medicine> medicines = pharmacy.getMedicines();
+            UIManager.displayData(medicines);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
