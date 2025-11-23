b8d0cbf20365da768d7fa00cbd1a7e299b5f6aa5
Dyestrm <darkyissus29@gmail.com>
2025-11-23 15:53:15 +0800

    feat: Implement `addCustomerAccount` and `addPharmacyAccount` methods

--- src/main/java/com/example/classes/Admin.java
+++ src/main/java/com/example/classes/Admin.java
@@ -1,9 +1,13 @@
 package com.example.classes;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Admin extends Account {
+    private List<Customer> customers;
+    private List<Pharmacy> pharmacies;
     
     @JsonCreator
     Admin(
@@ -11,5 +15,30 @@ public class Admin extends Account {
         @JsonProperty("username") String username,
         @JsonProperty("password") String password) {
         super(name, username, password);
+    }
+
+    // CREATE
+    // Method to create a new customer account
+    public void addCustomerAccount() {
+        System.out.println("Enter Customer details.");
+        String name = InputHandler.readInput("Name: ");
+        String username = InputHandler.readInput("Username: ");
+        String password = InputHandler.readInput("Password: ");
+        List<Medicine> medicines = List.of();
+
+        Customer newCustomer = new Customer(name, username, password, medicines, 0);
+        Database.createAccount(newCustomer);
+    }
+
+    // Method to create a new pharmacy account
+    public void addPharmacyAccount() {
+        System.out.println("Enter Pharmacy Details.");
+        String name = InputHandler.readInput("Name: ");
+        String username = InputHandler.readInput("Username: ");
+        String password = InputHandler.readInput("Password: ");
+        List<Medicine> medicines = List.of();
+
+        Pharmacy newPharmacy = new Pharmacy(name, username, password, medicines);
+        Database.createAccount(newPharmacy);
     }
 }
