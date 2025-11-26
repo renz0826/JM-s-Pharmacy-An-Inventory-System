96f7fe956c45939a5f0ed9012ad107692129eb10
Dyestrm <darkyissus29@gmail.com>
2025-11-26 18:09:42 +0800

    test: Test in Main

--- src/main/java/com/example/classes/Main.java
+++ src/main/java/com/example/classes/Main.java
@@ -2,6 +2,6 @@ package com.example.classes;
 
 public class Main {
     public static void main(String[] args) {        
-        UIManager.displayLoginChoice();
+        UIManager.chooseAccountMenu();
     }
 }
