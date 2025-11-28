315ab820879516b166ea997450fc5acb630d3bf4
Dyestrm <darkyissus29@gmail.com>
2025-11-28 18:54:03 +0800

    feat: Add start program function in main

--- src/main/java/com/jmpharmacyims/classes/Main.java
+++ src/main/java/com/jmpharmacyims/classes/Main.java
@@ -2,6 +2,11 @@ package com.jmpharmacyims.classes;
 
 public class Main {
     public static void main(String[] args) {        
+        startInventoryMedicineSystem();
+    }
+
+    private static void startInventoryMedicineSystem() {
+        Database.validateInitialDataFiles();
         UIManager.chooseAccountMenu();
     }
 }
